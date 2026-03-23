1f5af9eba6b6f184f28e6ccaeb13159051e086c9
medo <medopoplike@gmail.com>
2026-03-23 23:33:29 +0200

    Fix flatDir and update dependencies

---
+++ android/app/new.sh
@@ -0,0 +1,6 @@
+git add .
+    git commit -m "Fix flatDir and update dependencies"
+    git push origin main    git add .
+    git commit -m "Fix flatDir and update dependencies"
+    git push origin main
+    
